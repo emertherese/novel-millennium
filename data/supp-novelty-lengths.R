a50db6fd9916d82bf4a50c0a48a77f7148c66e99
emertherese <e.cunningham@uqconnect.edu.au>
2024-08-15 11:33:27 +1000

    Include data from past studies to compare novelty emergence rates

---
+++ data/supp-novelty-lengths.R
@@ -0,0 +1,23 @@
+pand2020 = readRDS(url("https://github.com/TimothyStaples/novelty-cenozoic-microplankton/raw/master/outputs/processedNovelData.rds"))
+
+pandLong = do.call("rbind", lapply(1:length(pand2020), function(x){
+  temp = do.call('rbind', pand2020[[x]]$novel)
+  temp$taxa = c("nano", "foram", "radio", "diatom")[x]
+  return(temp)
+}))
+
+# time series ratio (length to bin count)
+grainToExtent = tapply(pandLong$bins, list(pandLong$site, pandLong$taxa),
+       function(x){diff(range(as.numeric(as.character(x)))) / 0.1})
+hist(grainToExtent)
+summary(as.vector(grainToExtent))
+
+# Staples 2022
+Stap2022 <- readRDS(url("https://github.com/TimothyStaples/novel_comm_quaternary/raw/master/outputs/all%20neotoma%20novelty%20(sub-sampled).rds"))
+
+StapLong <- do.call("rbind", Stap2022$novel)
+
+grainToExtent = tapply(StapLong$bins, StapLong$site,
+                       function(x){diff(range(as.numeric(as.character(x)))) / 200})
+hist(grainToExtent)
+summary(as.vector(grainToExtent))
